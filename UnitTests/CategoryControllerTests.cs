7cb748af3e80546e5c08a39ec29181b0ba233958
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2022-02-28 21:34:17 +0000

    Update CategoryControllerTests.cs
    • Altered 'return' and 'Assert' in 'CategoryControllerDetailsIDNotEqual()' Test
    • Added Comments throughout Tests to provide sufficient code annotation

--- UnitTests/CategoryControllerTests.cs
+++ UnitTests/CategoryControllerTests.cs
@@ -11,7 +11,7 @@ namespace UnitTests
     public class CategoryControllerTests
     {
         private AppDbContext _db;
-        private CategoryModel _existingCategory;
+        private CategoryModel _existingCategory; //Variable used for storing existing category in Mock database for quick refrence in tests
 
         private void CreateMockDB()
         {
@@ -24,8 +24,9 @@ namespace UnitTests
 
         private async void PopulateMockDB()
         {
-            CreateMockDB();
+            CreateMockDB(); //Calls the function to create a mock database
 
+            //Three Categories, containing basic information, are all added to the Mock database
             var CategoryA = new CategoryModel() { Id = 1, Name = "Test Category A" };
             var CategoryB = new CategoryModel() { Id = 2, Name = "Test Category B" };
             var CategoryC = new CategoryModel() { Id = 3, Name = "Test Category C" };
@@ -36,13 +37,13 @@ namespace UnitTests
             await _db.SaveChangesAsync();
         }
 
-        private async void SetExistingCategory(int id)
+        private async void SetExistingCategory(int id) //Function that when called, sets the _existingCategory variable to a specified instance from the database
         {
             var categories = await _db.Categories.ToListAsync();
-            _existingCategory = categories.Find(x => x.Id == id);
+            _existingCategory = categories.Find(x => x.Id == id); //Sets the variable based on passed ID
         }
 
-        [Fact]
+        [Fact] //Test to check the Index area of the Category Controller is not null
         public void CategoryControllerIndexNotNull()
         {
             //Arrange : Create the mock database and controller
@@ -56,89 +57,112 @@ namespace UnitTests
             Assert.NotNull(result);
         }
 
-        [Fact]
+        [Fact] //Test to check the details of a category can be retrievd through the controller
         public async void CategoryControllerDetailsRetrievalSuccessfull()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
 
+            //Act : Produce a variable that can be assessed - the result of attempting to view details of category in database, based on ID
             var result = await controller.Details(1) as ViewResult;
-
             SetExistingCategory(1);
 
+            //Assert : Check the reuruned data is correct, comparing the model within the returned model to the actual database entry
             Assert.Equal(result.Model, _existingCategory);
         }
 
-        [Fact]
+        [Fact] //Test to validate controller correctly handles error from attempting to view detail of non-existent category
         public async void CategoryControllerDetailsIDNotEqual()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
 
-            var result = await controller.Details(0) as ViewResult;
+            //Act : Produce a variable that can assessed - the result of attempting to view details with incorrect ID
+            var result = await controller.Details(0) as NotFoundResult;
 
-            SetExistingCategory(1);
-
-            Assert.Equal(result.Model, _existingCategory);
+            //Assert : Check the result matches the expected outcome, with status code 404 indicating the category could not be found
+            Assert.Equal("404", result.StatusCode.ToString());
         }
 
-        [Fact]
+        [Fact] //Test to check if a new category can be added to the database through the controller
         public async void CategoryControllerCreateSuccessfull()
         {
+            //Arrange : Create the mock database and controller
             CreateMockDB();
             var controller = new CategoryController(_db);
-            var newCategory = new CategoryModel() { Id = 1, Name = "Created Product"};
 
+            //Sets details of the category to be created
+            var newCategory = new CategoryModel() { Id = 30, Name = "Created Product"};
+
+            //Act: Produce a variable that can be assessed - the result of attempting to create a new category
             var result = await controller.Create(newCategory);
 
+            //Assert : Check the result matches the outcome, test will pass if passed category is now in the database
             Assert.NotNull(result);
             Assert.Contains(newCategory, _db.Categories);
         }
 
-        [Fact]
+        [Fact] //Test to validate controller correctly handles an attempt to create new category with identical ID to an existing entry
         public async void CategoryControllerCreateExistingCategory()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
-            var newCategory = new CategoryModel() { Id = 1, Name = "Test Category A" };
 
+            //Sets the details of the category to be added as the same as an existing database entry
+            SetExistingCategory(1);
+            var newCategory = _existingCategory;
+
+            //Act: Produce a variable that can be assessed - the result of attempting to create a new category using invalid details
             var result = await controller.Create(newCategory);
 
+            //Assert : Check the result matches the outcome, test will pass if a message is returned
             Assert.NotNull(result);
         }
 
-        [Fact]
+        [Fact] //Test to check existing categories can be edited through the controller
         public async void CategoryControllerEditSuccessfull()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
             SetExistingCategory(1);
 
+            //Act: Produce a variable that can be assessed - the result of attempting to edit an existing category
             var result = await controller.Edit(_existingCategory.Id) as ViewResult;
 
+            //Assert : Check the result matches the outcome, test passes if a view is returned (This view would then be used in the front-end to amend details)
             Assert.NotNull(result.ViewData);
         }
 
-        [Fact]
+        [Fact] //Test to validate controller correctly handles an attempt to edit a non-existent category
         public async void CategoryControllerEditIDNotEqual()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
 
+            //Act: Produce a variable that can be assessed - the result of attempting to edit a non-existent category
             var result = await controller.Edit(12) as NotFoundResult;
 
+            //Assert : Check the result matches the outcome, with status code 404 indicating the category could not be found
             Assert.Equal("404", result.StatusCode.ToString());
         }
 
-        [Fact]
+        [Fact] //Test to check existing categories can be successfully deleted from the database through the controller
         public async void CategoryControllerDeleteSuccessful()
         {
+            //Arrange : Create the mock database and controller
             PopulateMockDB();
             var controller = new CategoryController(_db);
             SetExistingCategory(1);
 
+            //Act: Produce a variable that can be assessed - the result of attempting to delete a valid category
             var result = await controller.DeleteConfirmed(_existingCategory.Id);
 
+            //Assert : Check the result matches the outcome, the test passes if the category is no longer present in the database
             Assert.NotNull(result);
             Assert.DoesNotContain(_existingCategory, _db.Categories);
         }
