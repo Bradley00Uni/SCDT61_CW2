2c47d537d62bd5f9d21461147398b4a33046f7fa
Bradley <dea18001355@btc.ac.uk>
2022-03-02 10:36:38 +0000

    Publish To Azure
    Adds .yml file for use with Microsoft Azure

--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
-using Xunit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace SeleniumTests
 {
-    public class UnitTest1
+    public class Tests
     {
-        [Fact]
+        IWebDriver driver = new ChromeDriver("D\\Level 6\\SCDT61 -  Software Development & QA\\Assignments\\Assignment 2\\chromedriver");
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
         public void Test1()
         {
-
+            Assert.Pass();
         }
     }
 }
