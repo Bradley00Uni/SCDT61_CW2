40c26be176e562da31220b6feeb592188125e5d9
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2022-04-04 19:14:12 +0100

    Create ShoppingCartManagementTests.cs
    Two Selenium Tests, each for testing how quantity is handled within the Shopping Cart.
    
    Both tests pass, meaning the cart can handle a quantity of 100+ and that the navbar's quantity total is accurately updated

---
+++ SeleniumTests/ShoppingCartManagementTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace SeleniumTests
+{
+    public class ShoppingCartManagementTests
+    {
+        private string loginPassword;
+        private string loginEmail;
+
+        IWebDriver driver = new ChromeDriver();
+
+        private void PopulateCustomer()
+        {
+            loginPassword = "Customer123!";
+            loginEmail = "customer@customer.com";
+        }
+        private void Login()
+        {
+            driver.Navigate().GoToUrl("https://onlineshop202220220302112626.azurewebsites.net/Identity/Account/Login");
+
+            PopulateCustomer();
+
+            IWebElement emailInput = driver.FindElement(By.Id("Input_Email"));
+            emailInput.SendKeys(loginEmail);
+            IWebElement passwordInput = driver.FindElement(By.Id("Input_Password"));
+            passwordInput.SendKeys(loginPassword);
+            emailInput.SendKeys(Keys.Return);
+            Thread.Sleep(1000);
+        }
+        public void Logout()
+        {
+            IWebElement logoutButton = driver.FindElement(By.Id("logout"));
+            logoutButton.Click();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            driver.Navigate().GoToUrl("https://onlineshop202220220302112626.azurewebsites.net/");
+            driver.Manage().Window.Maximize();
+            Login();
+        }
+
+        [Test]
+        public void CartHandleQuantityOver100()
+        {
+            var quantity = 0;
+
+            do
+            {
+                driver.FindElement(By.Id("product 2")).FindElement(By.ClassName("btn")).Click();
+                driver.Navigate().GoToUrl("https://onlineshop202220220302112626.azurewebsites.net/");
+                quantity++;
+            }while (quantity < 99);
+
+            driver.FindElement(By.Id("product 2")).FindElement(By.ClassName("btn")).Click();
+
+            if (driver.FindElement(By.ClassName("table")).Text.Contains("100"))
+            {
+                Logout();
+                Assert.Pass();
+            }
+            else
+            {
+                Console.WriteLine("Not All Products Accounted For");
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void CardTotalCorrectlyUpdates()
+        {
+            var quantity = 0;
+
+            do
+            {
+                driver.FindElement(By.Id("product 2")).FindElement(By.ClassName("btn")).Click();
+                driver.Navigate().GoToUrl("https://onlineshop202220220302112626.azurewebsites.net/");
+                quantity++;
+            } while (quantity < 10);
+
+            if (driver.FindElement(By.Id("cart-status")).Text == "10")
+            {
+                Logout();
+                Assert.Pass();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Quantity Shown");
+                Assert.Fail();
+            }
+        }
+
+        [OneTimeTearDown]
+        public void End() { driver.Close(); }
+    }
+}
